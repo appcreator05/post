b7e328e4802bcc367f4c781ef4ca70d1859925d6
appcreator05 <installapkapps@gmail.com>
2026-07-08 17:25:42 +0530

    Refactor getNewAccessToken and postToBlogger functions

--- post.js
+++ post.js
@@ -2,13 +2,22 @@ const https = require('https');
 
 async function getNewAccessToken() {
     const data = `client_id=${process.env.G_CLIENT_ID}&client_secret=${process.env.G_CLIENT_SECRET}&refresh_token=${process.env.G_REFRESH_TOKEN}&grant_type=refresh_token`;
-    return new Promise((resolve) => {
-        const req = https.request({hostname: 'oauth2.googleapis.com', path: '/token', method: 'POST', headers: {'Content-Type': 'application/x-www-form-urlencoded'}}, (res) => {
+    return new Promise((resolve, reject) => {
+        const req = https.request({
+            hostname: 'oauth2.googleapis.com',
+            path: '/token',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }, (res) => {
             let body = '';
             res.on('data', (d) => { body += d; });
-            res.on('end', () => { resolve(JSON.parse(body).access_token); });
+            res.on('end', () => {
+                const json = JSON.parse(body);
+                json.access_token ? resolve(json.access_token) : reject(json);
+            });
         });
-        req.write(data); req.end();
+        req.write(data);
+        req.end();
     });
 }
 
@@ -22,21 +31,32 @@ async function postToBlogger(movie, token) {
         hostname: 'www.googleapis.com',
         path: `/blogger/v3/blogs/${process.env.BLOG_ID}/posts/`,
         method: 'POST',
-        headers: {'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json'}
+        headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' }
     };
-    const req = https.request(options, (res) => {
-        console.log(`Posted: ${movie.title} - Status: ${res.statusCode}`);
+    return new Promise((resolve) => {
+        const req = https.request(options, (res) => {
+            console.log(`Posted: ${movie.title} - Status: ${res.statusCode}`);
+            resolve();
+        });
+        req.write(postData);
+        req.end();
     });
-    req.write(postData); req.end();
 }
 
 (async () => {
-    const token = await getNewAccessToken();
-    https.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}`, (res) => {
-        let data = '';
-        res.on('data', (chunk) => { data += chunk; });
-        res.on('end', () => {
-            JSON.parse(data).results.forEach(m => postToBlogger(m, token));
+    try {
+        const token = await getNewAccessToken();
+        https.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}`, (res) => {
+            let data = '';
+            res.on('data', (chunk) => { data += chunk; });
+            res.on('end', async () => {
+                const movies = JSON.parse(data).results;
+                for (const movie of movies) {
+                    await postToBlogger(movie, token);
+                }
+            });
         });
-    });
+    } catch (e) {
+        console.error("Error: ", e);
+    }
 })();
